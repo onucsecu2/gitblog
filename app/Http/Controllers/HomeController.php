5e0ccecc82e87cee2a331c86a89d7f665ef0d9bd
unknown <onucsecu@gmail.com>
2020-08-12 03:13:05 +0600

    database migration order and  typho fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,8 @@ class HomeController extends Controller
     {
         return view('home');
     }
+    public function logout(){
+        Auth::logout();
+        return redirect('/login');
+    }
 }
